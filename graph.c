0089363a049140610f0e3a55893f81074424c4e9
dani4655 <dani4655@gmail.com>
2021-12-29 21:40:31 +0200

    fixes from the debugger

--- graph.c
+++ graph.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "graph.h"
+pnode getNode(pnode* head, int id);
 
 
 void build_graph_cmd(pnode *head) {
@@ -9,7 +10,7 @@ void build_graph_cmd(pnode *head) {
     scanf("%d", &nodesize);
     char n = 'n';
     int x = 0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < nodesize; i++) {
         scanf("%c", &n);
         insert_node_cmd(head);
     }
@@ -22,14 +23,14 @@ void insert_node_cmd(pnode *head) {
     pnode n = getNode(head,id);
     
     if (n == NULL) {
-        n = (pnode)malloc(sizeof(node))
+        n = (pnode)malloc(sizeof(node));
         n->node_num = id;
         n->edges = NULL;
-        n->next = head; //put n befor head
+        n->next = *head; //put n befor head
         *head = n; // set n to be new head
     }
     else { //if node exist
-        pedge e = n->edges
+        pedge e = n->edges;
         while (e != NULL) {
             pedge t = e->next;
             free(e);
@@ -38,13 +39,13 @@ void insert_node_cmd(pnode *head) {
     }
     int destid = -1;
     while(scanf("%d", &destid) != NULL) {
-        dest = getNode(head,destid);
+        pnode dest = getNode(head,destid);
         int w = -1;
         scanf("%d", &w);
-        edg = (pedge)malloc(sizeof(edge));
+        pedge edg = (pedge)malloc(sizeof(edge));
         edg->weight = w;
         edg->endpoint = dest;
-        edg->next = ;
+        edg->next = NULL;
     }
     
 }
