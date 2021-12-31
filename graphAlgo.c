dc7cb8cb1403d884515edff6212bd45ebd8d9a0e
dani4655 <dani4655@gmail.com>
2021-12-31 15:55:50 +0200

    fixes from the debugger

--- graphAlgo.c
+++ graphAlgo.c
@@ -1,10 +1,12 @@
 #include "graph.h"
 #include <stdlib.h>
 #include <stdio.h>
+#define infinity 20000000
 typedef struct graph_copy {
     int node_num;
     pedge edges;
     int tag;
+    int weight;
     struct graph_copy *next;
 } copy, *pcopy;
 
@@ -21,8 +23,14 @@ pcopy copyg(pnode *head, int size){
 void maxVal(pcopy *head){
     while((*head)->next!=NULL){
         (*head)->tag=-1;
-        (*head)->
+        (*head)->weight= infinity;
     }
 }
+
+void relax(pedge e, pcopy c){
+    if(c->weight > e->endpoint->edges->weight)
+}
+
+
 
 
